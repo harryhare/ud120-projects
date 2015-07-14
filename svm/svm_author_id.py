3b883362fbfba089ba76a71b98b2af2d521e3ae0
harryhare <harryhare@163.com>
2015-07-14 09:22:36 +0800

    add code to svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,12 +21,44 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train=features_train[0:len(features_train)/100]
+#labels_train=labels_train[0:len(labels_train)/100]
 
+from sklearn.svm  import SVC
+def data_process(C):
+	clf=SVC(C,kernel='rbf')
+	t0=time()
+	clf.fit(features_train,labels_train)
+	print("trainint time:",round(time()-t0,3),'s')
+	t0=time()
+	accuracy=clf.score(features_test,labels_test) 
+	print("testint time:",round(time()-t0,3),'s')
+	print("C=",C,",accuracy=",accuracy)
 
 
 #########################################################
 ### your code goes here ###
+'''
+data_process(1.0)
+data_process(10.0)
+data_process(100.0)
+data_process(1000.0)
+data_process(10000.0)
+data_process(100000.0)
+data_process(1000000.0)
+'''
 
+
+#data_process(10000.0)
+clf=SVC(10000,kernel='rbf')
+t0=time()
+clf.fit(features_train,labels_train)
+print("trainint time:",round(time()-t0,3),'s')
+t0=time()
+prediction=clf.predict(features_test) 
+print("testint time:",round(time()-t0,3),'s')
+print(prediction.sum())
+	
 #########################################################
 
 
