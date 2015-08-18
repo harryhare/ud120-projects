7637cee3f862455498a76aa94574c3360c4d5906
harryhare <harryhare@163.com>
2015-08-18 22:01:00 +0800

    add three type of feature choise

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+delete_uncomplete_data = False
+show_log = True
+
+def myprint(*argument):
+	if(show_log):
+		print(argument)
+
 import sys
 import pickle
 sys.path.append("../tools/")
@@ -16,6 +23,8 @@ features_list = ['poi','salary'] # You will need to use more features
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
 
 '''my code begin'''
+data_dict.pop("TOTAL") #get off outlier
+
 poi_count=0
 names=data_dict.keys()
 features=data_dict[names[0]].keys()
@@ -23,6 +32,20 @@ for name in names:
 	if(data_dict[name]['poi']==True):
 		poi_count+=1
 print(poi_count)
+
+new_feature1="fraction_from_poi_to_this_person"
+new_feature2="fraction_from_this_person_to_poi"
+for name in names:
+	person=data_dict[name]
+	if(person['to_messages']!="NaN" and person['from_messages']!="NaN"):
+		person[new_feature1]=(person["from_poi_to_this_person"]+0.)/person['to_messages']
+		person[new_feature2]=(person["from_this_person_to_poi"]+0.)/person['from_messages']
+	else:
+		person[new_feature1]="NaN"
+		person[new_feature2]="NaN"
+
+
+		
 
 key_person1='LAY KENNETH L'
 key_person2='SKILLING JEFFREY K'
@@ -66,17 +89,88 @@ for name in names:
         person=data_dict[name]
         if(person['poi']==True):
                 print(name,person['other'])
-'''
-delete_count=0
-for name in names:
-        for features in good_features:
-                if(data_dict[name][feature]=="NaN"):
-                        data_dict.pop(name)
-                        delete_count+=1
-                        break
-print("delete %d person, due to uncomplete information" % delete_count)
-'''                
 
+if(delete_uncomplete_data):
+	delete_count=0
+	for name in names:
+			for features in good_features:
+					if(data_dict[name][feature]=="NaN"):
+							data_dict.pop(name)
+							delete_count+=1
+							break
+	print("delete %d person, due to uncomplete information" % delete_count)
+
+#due to so little features left
+#now I ignore some pois who hava uncomplete infomation
+#find poi with uncomplete feature
+pois=[]
+for name in names:
+	if(data_dict[name]['poi']==True):
+		pois.append(name)
+print(len(pois))
+print(pois)
+
+
+
+def find_poi_without_feature(feature):
+	names=[]
+	for name in pois:
+		if(data_dict[name][feature]=='NaN'):
+			names.append(name)
+	myprint(feature,names);
+	return names
+
+feature_list1=["salary","restricted_stock"]
+'''
+decisiontree recall,precisio 0.2
+'''
+feature_list2=feature_list1+["bonus"]
+'''
+DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
+            max_features=None, max_leaf_nodes=None, min_samples_leaf=1,
+            min_samples_split=5, min_weight_fraction_leaf=0.0,
+            random_state=None, splitter='best')
+	Accuracy: 0.83787	Precision: 0.35919	Recall: 0.27550	F1: 0.31183	F2: 0.28897
+AdaBoostClassifier(algorithm='SAMME.R', base_estimator=None,
+          learning_rate=1.0, n_estimators=50, random_state=None)
+	Accuracy: 0.85113	Precision: 0.41503	Recall: 0.28450	F1: 0.33759	F2: 0.30360
+	Total predictions: 15000	True positives:  569	False positives:  802	False negatives: 1431	True negatives: 12198
+'''
+feature_list3=feature_list2+["to_messages","from_messages",
+ "from_poi_to_this_person","from_this_person_to_poi","shared_receipt_with_poi",
+  new_feature1,new_feature2]
+'''
+After scaler:
+DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
+            max_features=None, max_leaf_nodes=None, min_samples_leaf=1,
+            min_samples_split=6, min_weight_fraction_leaf=0.0,
+            random_state=None, splitter='best')
+	Accuracy: 0.93057	Precision: 0.51163	Recall: 0.61600	F1: 0.55898	F2: 0.59185
+[('fraction_from_poi_to_this_person', 0.45804342800706016), 
+('total_stock_value', 0.23487292711823565),
+ ('expenses', 0.12970722666215376),
+ ('from_this_person_to_poi', 0.12954555710659485),
+ ('total_payments', 0.047830861105955622),
+ ('shared_receipt_with_poi', 0.0), 
+ ('from_poi_to_this_person', 0.0),
+ ('from_messages', 0.0), 
+ ('to_messages', 0.0),
+ ('bonus', 0.0),
+ ('restricted_stock', 0.0),
+ ('salary', 0.0),
+ ('other', 0.0),
+ ('poi', 0.0)]
+'''
+feature_test=feature_list3 # can be tuned 
+
+name_set=set()
+for feature in  feature_test:
+	temp=find_poi_without_feature(feature)
+	name_set=name_set.union(set(temp))
+features_list+=feature_test
+for name in name_set:
+	data_dict.pop(name)
+	myprint("***delete:"+name)
 
 
 
@@ -94,6 +188,12 @@ my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+'''do min_max_scalar'''
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler()
+#scaler.fit_transform(features)
+'''done'''
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -134,9 +234,9 @@ param_grid = {
 process(GaussianNB,"naive_bayes",param_grid)
 
 param_grid = {
-         #'kernel': ['rbf','linear'],
-         #'C': [1, 1e2, 1e3, 1e4],
-         #'gamma': [0,0.0001, 0.001, 0.01, 0.1],
+         'kernel': ['rbf'],
+         'C': [1, 1e2],
+         'gamma': [0, 0.001, 0.01, 0.1],
           }
 process(SVC,"svm",param_grid)
 param_grid = {
@@ -160,6 +260,10 @@ param_grid = {
     }
 process(AdaBoostClassifier,"AdaBoost",param_grid)
 
+clf=DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=None,
+            max_features=None, max_leaf_nodes=None, min_samples_leaf=1,
+            min_samples_split=6, min_weight_fraction_leaf=0.0,
+            random_state=None, splitter='best')
 '''my code end'''
 
 
@@ -169,7 +273,7 @@ process(AdaBoostClassifier,"AdaBoost",param_grid)
 ### shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-#test_classifier(clf, my_dataset, features_list)
+test_classifier(clf, my_dataset, features_list)
 
 ### Dump your classifier, dataset, and features_list so 
 ### anyone can run/check your results.
